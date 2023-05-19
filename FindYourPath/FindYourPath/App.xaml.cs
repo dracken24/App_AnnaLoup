6de4408b367718b70b56ca7badd8c114ed841aa0
drack <drack@DRACKEN24-WIN>
2023-05-18 23:20:45 -0400

    Ajoutez des fichiers projet.

---
+++ FindYourPath/FindYourPath/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using FindYourPath.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FindYourPath
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			// main page start program
+			MainPage = new NavigationPage(new MainPage());
+		}
+
+		protected override void OnStart()
+		{
+		}
+
+		protected override void OnSleep()
+		{
+		}
+
+		protected override void OnResume()
+		{
+		}
+	}
+}
---
+++ FindYourPath/FindYourPath/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using FindYourPath.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FindYourPath
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		// On click, go to PersonnalInfos page
+		private void ProfileClicked(object sender, EventArgs e)
+		{
+			this.Navigation.PushAsync(new PersonnalInfos());
+        }
+
+		// On click, go to Agenda page
+		private void AgendaClicked(object sender, EventArgs e)
+		{
+			this.Navigation.PushAsync(new Agenda());
+		}
+
+		// On click, go to TodoList page
+		private void TodoListClicked(object sender, EventArgs e)
+		{
+			this.Navigation.PushAsync(new TodoList());
+		}
+
+		// On click, go to Encore page
+		private void EncoreClicked(object sender, EventArgs e)
+		{
+			this.Navigation.PushAsync(new Encore());
+		}
+
+		// On click, go to Ressources page
+		private void RessourcesClicked(object sender, EventArgs e)
+		{
+			this.Navigation.PushAsync(new Ressources());
+		}
+	}
+}
