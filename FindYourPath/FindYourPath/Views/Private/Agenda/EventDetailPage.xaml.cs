f2dba8f9aca176e982e4a1420c6f92e37c6b0071
dracken24 <dracken24@gmail.com>
2023-05-26 15:21:21 -0400

    save

--- FindYourPath/FindYourPath/Views/Private/Agenda/EventDetailPage.xaml.cs
+++ FindYourPath/FindYourPath/Views/Private/Agenda/EventDetailPage.xaml.cs
@@ -13,6 +13,7 @@ namespace FindYourPath.Views
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class EventDetailPage : ContentPage
 	{
+		
 		public EventDetailPage(ScheduleAppointment appointment)
 		{
 			Title = "Détail de l'événement";
@@ -22,13 +23,21 @@ namespace FindYourPath.Views
 			var endTimeLabel = new Label { Text = $"Heure de fin: {appointment.EndTime}" };
 			var notesLabel = new Label { Text = $"Notes: {appointment.Notes}" };
 
-			var stackLayout = new StackLayout
-			{
-				Children = { subjectLabel, startTimeLabel, endTimeLabel, notesLabel },
-				Padding = new Thickness(20)
-			};
-
-			Content = stackLayout;
+			subLabel.Text = subjectLabel.Text;
+			startTLabel.Text = startTimeLabel.Text;
+			endTLabel.Text = endTimeLabel.Text;
+			noLabel.Text = notesLabel.Text;
 		}
+		/*
+
+		public EventDetailPage(CommunityResource resource)
+		{
+			Title = resource.Name;
+
+			nameLabel.Text = $"Nom: {resource.Name}";
+			addressLabel.Text = $"Adresse: {resource.Address}";
+			phoneLabel.Text = $"Téléphone: {resource.PhoneNumber}";
+			typeLabel.Text = $"Type: {resource.Type}";
+		}*/
 	}
 }
