bbdde84818b311930bc43b68413c6a4ad07ae81e
dracken24 <dracken24@gmail.com>
2023-05-26 15:52:26 -0400

    calendar set frontend

--- FindYourPath/FindYourPath/Views/Private/Agenda/EventDetailPage.xaml.cs
+++ FindYourPath/FindYourPath/Views/Private/Agenda/EventDetailPage.xaml.cs
@@ -10,34 +10,22 @@ using Xamarin.Forms.Xaml;
 
 namespace FindYourPath.Views
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class EventDetailPage : ContentPage
 	{
-		
 		public EventDetailPage(ScheduleAppointment appointment)
 		{
+			InitializeComponent();
 			Title = "Détail de l'événement";
 
-			var subjectLabel = new Label { Text = $"Sujet: {appointment.Subject}" };
-			var startTimeLabel = new Label { Text = $"Heure de début: {appointment.StartTime}" };
-			var endTimeLabel = new Label { Text = $"Heure de fin: {appointment.EndTime}" };
-			var notesLabel = new Label { Text = $"Notes: {appointment.Notes}" };
+			var subjectLabel = new Label { Text = $"Sujet                   : {appointment.Subject}" };
+			var startTimeLabel = new Label { Text = $"Heure de début : {appointment.StartTime}" };
+			var endTimeLabel = new Label { Text = $"Heure de fin       : {appointment.EndTime}" };
+			var notesLabel = new Label { Text = $"Notes                  : {appointment.Notes}" };
 
 			subLabel.Text = subjectLabel.Text;
 			startTLabel.Text = startTimeLabel.Text;
 			endTLabel.Text = endTimeLabel.Text;
 			noLabel.Text = notesLabel.Text;
 		}
-		/*
-
-		public EventDetailPage(CommunityResource resource)
-		{
-			Title = resource.Name;
-
-			nameLabel.Text = $"Nom: {resource.Name}";
-			addressLabel.Text = $"Adresse: {resource.Address}";
-			phoneLabel.Text = $"Téléphone: {resource.PhoneNumber}";
-			typeLabel.Text = $"Type: {resource.Type}";
-		}*/
 	}
 }
